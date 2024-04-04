8190587f5089ec812c05a078e17ad5ee7450b79d
waltherbart <bartusalach@gmail.com>
2024-04-04 11:47:52 +0200

    zadanie domowe Zmienne i podstawowe operacje

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -6,17 +6,14 @@ if (gender == "kobieta" && age < 30)
 {
     Console.WriteLine("kobieta poniżej 30");
 }
-
 else if (name == "Ewa" && age == 30)
 {
     Console.WriteLine("Ewa, lat 30.");
 }
-
 else if (gender == "mężczyzna" && age < 18)
 {
     Console.WriteLine("Niepełnoletni mężczyzna.");
 }
-
 else
 {
     Console.WriteLine("NIC");
